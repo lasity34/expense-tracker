3fb9922b23342f88b3d47e6d054997cf89427d8d
Bjorn Worrall <85297118+lasity34@users.noreply.github.com>
2023-11-29 14:20:51 +0200

    remove date

---
+++ db1.sql
@@ -0,0 +1,6 @@
+INSERT into category (category_type) values ('weekly');
+INSERT into category (category_type) values ('monthly');
+INSERT into category (category_type) values ('weekday');
+INSERT into category (category_type) values ('weekend');
+INSERT into category (category_type) values ('once-off');
+INSERT into category (category_type) values ('daily');
